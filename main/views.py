0010e4632776c8baebb6e182da56044f81aeeac9
miguelpaday <miguelpaday12@gmail.com>
2019-11-20 01:19:21 +0400

    Added Bootstrap, Logo, Navbar

--- main/views.py
+++ main/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>Welcome, Miguel!<h1>")
+def index(request):
+    return render(request, 'main/base.html')
 
 def v1(response):
     return HttpResponse("View 1")
